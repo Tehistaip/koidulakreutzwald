4ef8b62dbf7ae0b041320dca7b1d678a9416a9fa
Neeme Kahusk <nemee@ut.ee>
2024-06-15 22:37:54 +0300

    uued vertiga failid

---
+++ tei/koidulakreutzwald.sql
@@ -0,0 +1,1 @@
+SET @@session.sql_mode ='ALLOW_INVALID_DATES'; DELETE FROM `timedata` WHERE `corpus` = 'KOIDULAKREUTZWALD'; DELETE FROM `timedata_date` WHERE `corpus` = 'KOIDULAKREUTZWALD'; SET NAMES utf8;
